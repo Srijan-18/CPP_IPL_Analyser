867bc9497c1e2f306927e16d7f1f3f4405cd3d8d
Srijan-18 <srijan.mishrasir@gmail.com>
2020-09-11 05:55:30 +0530

    Welecome message.

---
+++ controller/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void print_welcome_message();
+
+int main()
+{
+    print_welcome_message();
+}
+
+void print_welcome_message()
+{
+    cout << "\n\t\t***** WELCOME TO IPL ANALYSER *****\n" << endl;
+}
